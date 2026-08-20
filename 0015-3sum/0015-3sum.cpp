afd1701d4cca9eb638f1ab5d88066a2ce84aeb26
Yog Chouhan <yogchouhan1628@gmail.com>
2026-08-20 23:42:39 +0530

    Time: 49 ms (48.99%), Space: 31 MB (20.57%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,6 +5,7 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j = i + 1;
             int k = n - 1;
