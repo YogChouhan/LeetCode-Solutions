c6695fa40b89e669cba61720a639d340da3596ee
Yog Chouhan <yogchouhan1628@gmail.com>
2026-08-23 20:33:00 +0530

    Time: 43 ms (57.46%), Space: 51 MB (19.1%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        map<int,int> hashMap;
+        unordered_map<int,int> hashMap;
         int cnt=0,prefixSum=0;
         hashMap[0]=1;
         for(int i: nums){
