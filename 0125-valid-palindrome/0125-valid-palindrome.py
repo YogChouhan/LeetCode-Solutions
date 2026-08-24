901d15a59ff76676add0744659583039e03cc36a
Yog Chouhan <yogchouhan1628@gmail.com>
2026-08-24 19:44:23 +0530

    Time: 3 ms (99.03%), Space: 18.3 MB (100%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ns=''
+        for i in s:
+            if i.isalnum():
+                ns+=i.lower()
+
+        if ns==ns[::-1]:
+            return True
+        else:
+            return False
