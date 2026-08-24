5da973808f5cc2a448c2b2240a535de327d6af05
Yog Chouhan <yogchouhan1628@gmail.com>
2026-08-24 15:14:16 +0530

    Time: 0 ms (100%), Space: 65 MB (69.96%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int> hashMap;
-        hashMap[nums[0]]=1;
-        for(int i=1; i<nums.size(); i++){
-            if(hashMap.find(nums[i])!=hashMap.end()){
-                return nums[i];
-            }
-            hashMap[nums[i]]+=1;
+        int slow=nums[0], fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+
+        slow=nums[0];
+
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
         }
-        return -1;
+        return slow;
     }
 };
