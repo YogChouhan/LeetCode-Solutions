304f1c04397075589e05c893abb372fedb76819d
Yog Chouhan <yogchouhan1628@gmail.com>
2026-08-28 14:32:34 +0530

    Time: 0 ms (100%), Space: 17.8 MB (66.14%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        int j=num.size()-1,i=0;
+        int j=num.size()-1;
         while(j>=0){
             if((num[j]-'0')%2!=0){
                 break;
             }
             j--;
         }
-        string ans=num.substr(i, j-i+1);
+        string ans=num.substr(0, j+1);
         return ans;
     }
 };
