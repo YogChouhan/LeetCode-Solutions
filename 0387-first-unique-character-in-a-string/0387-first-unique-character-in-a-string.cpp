4034ac889797d46ae164a1923369f6e814ece7a1
Yog Chouhan <yogchouhan1628@gmail.com>
2026-09-01 23:55:38 +0530

    Add alternative implementation for first unique character
    Added commented-out implementation using a queue for data stream handling.

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -13,3 +13,23 @@ public:
         return -1;
     }
 };
+
+//implementing using queue when you have DATA STREAM
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         queue<int> q;
+//         int seen[26]={0};
+//         for(int i = 0; i < s.length(); i++){
+//             seen[s[i]-'a']++;
+//             q.push(i);
+//         }
+//         while(!q.empty()){
+//             if(seen[s[q.front()]-'a']==1){
+//                 return q.front();
+//             }
+//             else q.pop();
+//         }
+//         return -1;
+//     }
+// };
