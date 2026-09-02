e19ca9975a0b3d6e79f4e6bd27944f40d1ca0148
Yog Chouhan <yogchouhan1628@gmail.com>
2026-09-02 11:01:30 +0530

    Time: 9 ms (53.65%), Space: 20.6 MB (6.96%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+        queue<int> q;
         int seen[26]={0};
         for(int i = 0; i < s.length(); i++){
             seen[s[i]-'a']++;
+            q.push(i);
         }
-        for(int i = 0; i < s.length(); i++){
-            if(seen[s[i]-'a']==1){
-                return i;
+        while(!q.empty()){
+            if(seen[s[q.front()]-'a']==1){
+                return q.front();
             }
+            else q.pop();
         }
         return -1;
     }
 };
-
-//implementing using queue when you have DATA STREAM
-// class Solution {
-// public:
-//     int firstUniqChar(string s) {
-//         queue<int> q;
-//         int seen[26]={0};
-//         for(int i = 0; i < s.length(); i++){
-//             seen[s[i]-'a']++;
-//             q.push(i);
-//         }
-//         while(!q.empty()){
-//             if(seen[s[q.front()]-'a']==1){
-//                 return q.front();
-//             }
-//             else q.pop();
-//         }
-//         return -1;
-//     }
-// };
